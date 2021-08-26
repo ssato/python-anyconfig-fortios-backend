ca4f87a587af7a303cf0bb1bcb3a55dab1b424af
Satoru SATOH <satoru.satoh@gmail.com>
2021-08-26 13:14:03 +0900

    change: drop py2 support

--- src/anyconfig_fortios_backend/fortios.py
+++ src/anyconfig_fortios_backend/fortios.py
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020 Satoru SATOH <satoru.satoh@gmail.com>.
+# Copyright (C) 2020, 2021 Satoru SATOH <satoru.satoh@gmail.com>.
 # SPDX-License-Identifier: MIT
 #
 r"""Very experimental parser parses fortios configuration.
