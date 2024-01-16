aab6b174b32eae6a7b4534c398352db07a562f0b
Satoru SATOH <satoru.satoh@gmail.com>
2024-01-17 02:26:44 +0900

    change: parametrized test cases for load and dump APIs

--- tests/test_fortios.py
+++ tests/test_fortios.py
@@ -15,9 +15,6 @@ import anyconfig_fortios_backend.fortios as F
 import tests.constants
 
 
-_CNF_FILES = [str(p) for p in tests.constants.CNF_FILES]
-
-
 @pytest.mark.parametrize(
     ("line", "exp"),
     (("", None),
@@ -48,29 +45,35 @@ def test__process_set_multiline_value_end(line, exp):
     assert res == exp
 
 
-def test_load(tmp_path):
-    assert _CNF_FILES
+_CNF_FILES = [str(p) for p in tests.constants.CNF_FILES]
+_IPATH_EPATH_PAIRS = [
+    (p, p.parent / p.name.replace('.txt', '.json'))
+    for p in tests.constants.CNF_FILES
+]
+assert _IPATH_EPATH_PAIRS
 
-    for in_path in _CNF_FILES:
-        exp_path = in_path.replace('.txt', '.json')
-        out_path = tmp_path / 'out.json'
 
-        if not out_path.exists():
-            continue  # The reference test data is not ready.
+@pytest.mark.parametrize(
+    ("ipath", "epath"),
+    _IPATH_EPATH_PAIRS,
+    ids=[p.name for p, _e in _IPATH_EPATH_PAIRS],
+)
+def test_load(ipath, epath, tmp_path):
+    out_path = tmp_path / 'out.json'
 
-        try:
-            psr = TT.Parser()
-            cnf = psr.load(anyconfig.ioinfo.make(in_path),
-                           ac_ordered=True)
-            assert cnf
-            assert isinstance(cnf, F.DEF_DICT)
+    try:
+        cnf = TT.Parser().load(anyconfig.ioinfo.make(ipath), ac_ordered=True)
+        assert cnf
+        assert isinstance(cnf, F.DEF_DICT)
 
-            anyconfig.dump(cnf, out_path)
-            ocnf = anyconfig.load(out_path, ac_ordered=False)
-            exp = anyconfig.load(exp_path, ac_ordered=False)
-            assert ocnf == exp, f'{ocnf!r} vs. {exp!r}'
+        anyconfig.dump(cnf, out_path)
 
-        except AssertionError as exc:
-            raise AssertionError(f'file: {in_path}, exc={exc!s}')
+        ocnf = anyconfig.load(out_path, ac_ordered=False)
+        exp = anyconfig.load(epath, ac_ordered=False)
+
+        assert ocnf == exp, f'{ocnf!r} vs. {exp!r}'
+
+    except AssertionError as exc:
+        raise AssertionError(f'file: {ipath}, exc={exc!s}') from exc
 
 # vim:sw=4:ts=4:et:
